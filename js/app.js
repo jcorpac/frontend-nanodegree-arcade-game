5db2ca1c8bbd16ad09454afc7a47de1d7a817095
jcorpac <jcorpac@gmail.com>
2018-02-22 15:36:33 -0600

    Added Starting Enemy and Player Behavior Assigned new objects to fix rendering errors in initial code Set initial position for player Set initial positions for enemies Set initial speed for enemies Added code to move enemies and reuse enemy objects once offscreen.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
+var tileHeight = 80;
+var tileWidth = 101;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.initialLocation();
+    this.setEnemySpeed();
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +19,16 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // If the enemy is offscreen to the right, reset its 
+    // location to a random row on the left and assign a
+    //  random speed.
+    if (this.x >= 606) {
+        this.initialLocation();
+        this.setEnemySpeed();
+    }
+    // Move the enemy to the right at its assigned speed.
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +36,46 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Enemy appears one tile width offscreen to the left, on
+// a random row between 1 and 3.
+Enemy.prototype.initialLocation = function() {
+    var topEnemyRow = 1,
+        numEnemyRows = 3;
+    this.x = -tileWidth;
+    this.y = Math.floor(Math.random() * numEnemyRows + topEnemyRow) * tileHeight;
+};
+
+// Enemy's speed is random between 20 and 100
+Enemy.prototype.setEnemySpeed = function() {
+    this.speed = (Math.random() * 80) + 20;
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.initialLocation();
+};
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
+Player.prototype.update = function() {
+
+};
+
+Player.prototype.initialLocation = function() {
+    this.x = 2 * tileWidth;
+    this.y = 5 * tileHeight;
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy()];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
